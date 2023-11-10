2db039c4c98f2748003960f6de9e338fffcf90f4
Ewa Słota <ewa4852@gmail.com>
2023-11-10 22:58:14 +0100

    constructor params

--- src/nightworking/Square.java
+++ src/nightworking/Square.java
@@ -3,15 +3,15 @@ package nightworking;
 public class Square {
     int side;
 
-    public Square(int a) {
-        side = a;
+    public Square(int side) {
+        this.side = side;
     }
 
     public void presentCalculations() {
-        System.out.println("Kwadrat o boku " + side);
-        System.out.println("Pole powierzchni to: " + calculateField());
-        System.out.println("Obwód to: " + calculateCircuit());
-        System.out.println("Długość przekątnej to: " + calculateDiagonal());
+        System.out.println("Kwadrat o boku " + this.side);
+        System.out.println("Pole powierzchni to: " + this.calculateField());
+        System.out.println("Obwód to: " + this.calculateCircuit());
+        System.out.println("Długość przekątnej to: " + this.calculateDiagonal());
         System.out.println();
     }
 
