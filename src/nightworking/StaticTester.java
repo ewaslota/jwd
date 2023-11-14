1156cf0353c4efeaf494d09115d12d39b0303c8c
Ewa Słota <ewa4852@gmail.com>
2023-11-14 23:40:00 +0100

    nightworking/StaticTester

--- src/nightworking/StaticTester.java
+++ src/nightworking/StaticTester.java
@@ -3,8 +3,13 @@ package nightworking;
 public class StaticTester {
     public static String thing = "drzewo";
     public static int hight = 8;
-    public static void main(String[] args) {
+
+    public static void view() {
         System.out.print("Koło domu znajduje się " + StaticTester.thing);
         System.out.println(" jego wysokość to: " + StaticTester.hight + "m");
+    }
+
+    public static void main(String[] args) {
+        StaticTester.view();
     }
 }
