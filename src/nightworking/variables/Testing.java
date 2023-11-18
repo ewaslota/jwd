b232ba86a0037f8fad4fa4788cdb58d7a43c0365
Ewa Słota <ewa4852@gmail.com>
2023-11-18 17:20:35 +0100

    nightworking/varliables

---
+++ src/nightworking/variables/Testing.java
@@ -0,0 +1,24 @@
+package nightworking.variables;
+
+/**
+ * Zadeklaruj zmienną
+ * Inicjalizuj zmienną
+ * Zdefiniuj zmienną
+ */
+public class Testing {
+    public static void main(String[] args) {
+        String test;
+        test = "test1";
+
+        String test2 = "test2";
+
+        float speed;
+        speed = 3.3f;
+
+        float speed2 = 5.4f;
+
+        char dolar;
+        dolar = '$';
+        char percent = '%';
+    }
+}
