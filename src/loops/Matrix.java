a80ca4c048134b9c7b52d47e81d805103c3caf99
Ewa Słota <ewa4852@gmail.com>
2023-12-02 03:15:15 +0100

    nightworking/loops

--- src/loops/Matrix.java
+++ src/loops/Matrix.java
@@ -1,49 +1,54 @@
 package loops;
-//Napisz program, który dla dowolnej liczby całkowitej n z przedziału (1 ≤ n ≤ 30) wypisuje macierz
-// jak w poniższych przykładach. Użyj dwóch zagnieżdżonych pętli.
-//
-//        n = 2	matrix
-//        --------------
-//        1  2
-//        2  3
-//
-//        n = 3	matrix
-//        --------------
-//        1  2  3
-//        2  3  4
-//        3  4  5
-//
-//        n = 4	matrix
-//        --------------
-//        1  2  3  4
-//        2  3  4  5
-//        3  4  5  6
-//        4  5  6  7
 
 import java.util.Scanner;
 
+/**
+ * Napisz program, który dla dowolnej liczby całkowitej n z przedziału (1 ≤ n ≤ 30) wypisuje macierz jak w poniższych przykładach.
+ * Użyj dwóch zagnieżdżonych pętli.
+ *
+ * n = 2	matrix
+ * --------------
+ *   1  2
+ *   2  3
+ *
+ * n = 3	matrix
+ * --------------
+ *   1  2  3
+ *   2  3  4
+ *   3  4  5
+ *
+ * n = 4	matrix
+ * --------------
+ *   1  2  3  4
+ *   2  3  4  5
+ *   3  4  5  6
+ *   4  5  6  7
+ *
+ */
 public class Matrix {
     public static void main(String[] args) {
-        int n = 4;
-        System.out.println("n= " + n + " matrix");
-        System.out.println("-------------");
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= n; j++) {
-                System.out.print(i + j + " ");
-            }
+        Scanner scanner = new Scanner(System.in);
 
-                System.out.println();// wciśnij f 12 i wpisz liczbę n
+        System.out.print("Insert matrix size: ");
+        int n = scanner.nextInt();
 
+        System.out.println();
 
-
+        if (n < 1 || n > 30) {
+            System.out.println("Matrix size should be between 1 and 30!");
+            return;
         }
 
+        int value;
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                value = i + 1 + j;
+                System.out.print(value);
+                System.out.print(value < 10 ? "  " : " ");
+            }
+            System.out.print("\n");
+        }
     }
-   // wciśnij f 12 i wpisz liczbę n
-
-
-
-    }
-
+}
