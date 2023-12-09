7f4b5499ac736f19929f72bd4bfcba96156ee2bb
eslota <ewas4852@gmail.com>
2023-12-09 16:50:15 +0100

    Zjazd nr 3 dzień 1 sql

--- src/sqls/sql_laboratorium2.sql
+++ src/sqls/sql_laboratorium2.sql
@@ -1,1 +1,25 @@
 -- https://java.mmiklas.wszib.edu.pl/dodatki/lab-sql2.html
+USE eslota
+DROP SCHEMA IF EXISTS Forum;
+
+CREATE SCHEMA Forum;
+GO
+
+CREATE TABLE Topics (
+ID int
+ParentID int
+CreateTime datetime
+TopicSubject nvarchar(128)
+TopicBody nvarchar(MAX)
+AuthorID int
+ClueID int
+ViewsCnt int
+AnswersCnt int
+)
+
+CREATE TABLE UserStats (
+  ID int PRIMARY KEY,
+  UserID
+  TopicID
+  Times
+)
