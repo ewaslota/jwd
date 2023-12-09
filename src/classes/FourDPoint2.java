5dd77c1a6bdb41bae8e2454f7eb0f2513b0c566e
Ewa Słota <ewa4852@gmail.com>
2023-12-09 19:39:37 +0100

    jwd/classes

--- src/classes/FourDPoint2.java
+++ src/classes/FourDPoint2.java
@@ -7,11 +7,11 @@ public class FourDPoint2 extends Point {
     int t;
 
     public static void main(String[] args) {
-        FourDPoint2 fourDPoint2 = FourDPoint2.creata(1, 2, 3, 4);
+        FourDPoint2 fourDPoint2 = FourDPoint2.create(1, 2, 3, 4);
         fourDPoint2.show();
     }
 
-    static FourDPoint2 creata(int x, int y, int z, int t){
+    static FourDPoint2 create(int x, int y, int z, int t){
         FourDPoint2 fourDPoint2 = new FourDPoint2();
         fourDPoint2.x = x;
         fourDPoint2.y = y;
@@ -21,7 +21,7 @@ public class FourDPoint2 extends Point {
     }
 
     void show() {
-        System.out.println("x wynosi " + this.x);// this poprawia w tym miejscu czytelność ale nie jest konieczna
+        System.out.println("x wynosi " + this.x);// this poprawia w tym miejscu czytelność ale nie jest konieczne
         System.out.println("y wynosi " + this.y);
         System.out.println("z wynosi " + this.z);
         System.out.println("t wynosi " + this.t);
