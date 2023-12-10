e015aa4cdeaba2e51464fc12bf93c9f61cd5fe3c
Ewa Słota <ewa4852@gmail.com>
2023-12-10 08:41:49 +0100

    sqls/lab2.sql

--- src/sqls/sql_laboratorium2.sql
+++ src/sqls/sql_laboratorium2.sql
@@ -5,7 +5,7 @@ DROP SCHEMA IF EXISTS Forum;
 CREATE SCHEMA Forum;
 GO
 
-CREATE TABLE Topics (
+CREATE TABLE Forum.Topics (
 	ID int
 	ParentID int
 	CreateTime datetime
@@ -16,10 +16,24 @@ CREATE TABLE Topics (
 	ViewsCnt int
 	AnswersCnt int
 )
+GO
 
-CREATE TABLE UserStats (
-  ID int PRIMARY KEY,
-  UserID
-  TopicID
-  Times
+CREATE TABLE Forum.UserStats (
+  ID int
+  UserID int
+  TopicID int
+  Times datetimes
 )
+GO
+
+CREATE TABLE Forum.User (
+	ID int
+	EmailAddress int
+	Passt datetime
+	Firstname nvarchar(128)
+	Lastname nvarchar(MAX)
+	RegisterTime datetime
+	RegConfirmTime datetime
+	ForumPostsCnt int
+)
+GO
