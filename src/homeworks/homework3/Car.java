f4c91855bad169b219d8e78d64edb43c9f20f39e
Ewa Słota <ewa4852@gmail.com>
2024-02-17 08:25:47 +0100

    nauka w domu

--- src/homeworks/homework3/Car.java
+++ src/homeworks/homework3/Car.java
@@ -1,5 +1,13 @@
 package homeworks.homework3;
-import java.util.Random;
+/*
+Na parkingach Toyoty w trzech fabrykach nazwanych Motomachi, Tsutsumi, Tahara znajdują się parkingi z wyprodukowanymi samochodami. Modele samochów produkowanych ww fabrykach to Yaris, Corolla i Aygo w kolorach srebrnym, niebieskim i czarnym, produkowanych w latach 2018, 2019 i 2020. Pojemność parkingów w poszczególnych fabrykach to Motomachi - 5000, Tsutsumi - 4500, Tahara - 1700. Napisz program analizujący dane znajdujących się tam pojazdów wg wytycznych:
+samochód jako typ Car powinien przechowywać takie informacje jak nazwa modelu, kolor oraz rocznik,
+parkingi w odpowiednich fabrykach powinny być reprezentowane jako tablice typu Car,
+zapełnij prakingi samochodami w losowych konfiguracjach,
+wyświetl informacje ile srebrnych Toyot Yaris z rocznika 2020 znajduje się na parkingu fabryki Motomachi,
+wyświetl informacje jakiego koloru samochodów jest najwięcej na parkingu fabryki Tsutsumi,
+wyświetl informacje o pierwszych 10 samochodach na parkingu fabryki Tahara.
+ */
 
 public class Car {
 
